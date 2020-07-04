faff2744ba9ec06e4e1e9c4566760af760e95526
Lukasz Pudlo <l.pudlo1990@gmail.com>
2020-07-04 16:15:30 +0100

    Added my project

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const IndexPage = () => {
+    return (
+     <div>
+        <h1>Hello.</h1>
+        <h2>Glasgow is the second city of the Empire.</h2>
+    </div>
+	)
+}
+
+export default IndexPage
