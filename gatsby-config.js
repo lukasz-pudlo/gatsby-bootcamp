59a01938097b364a2ab9f9b89d0c654c3cb45855
Lukasz Pudlo <l.pudlo1990@gmail.com>
2020-07-05 23:41:29 +0100

    styling with scss components

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,10 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: 'Full-Stack Bootcamp',
+    author: 'Andrew Mead'
+  },
   plugins: [
     'gatsby-plugin-sass'
   ]
