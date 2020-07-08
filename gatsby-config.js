f113b03ba15cefb2f5bb8725cf78cd353f5f1f1b
Lukasz Pudlo <l.pudlo1990@gmail.com>
2020-07-08 11:34:46 +0100

    graphiQL and graphQL playground

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,9 +8,16 @@ module.exports = {
   /* Your site config here */
   siteMetadata: {
     title: 'Full-Stack Bootcamp',
-    author: 'Andrew Mead'
+    author: 'Lukasz Pudlo'
   },
   plugins: [
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+    {
+       resolve: 'gatsby-source-filesystem',
+       options: {
+          name: 'src',
+          path: `${__dirname}/src/`
+	   }
+	}
   ]
 }
---
+++ src/posts/gatsby.md
@@ -0,0 +1,12 @@
+---
+title: "The Great Gatsby Bootcamp"
+date: "2020-07-07"
+---
+
+I am learning Gatsby!
+
+## Topics Covered
+
+1. Gatsby
+2. GraphQL
+3. React
